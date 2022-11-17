ad02a54f5b7d8eb2cb4253a9277c8fb8118f2b1a
Your Name <namth365@gmail.com>
2022-11-18 01:19:07 +0700

    Done !!

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "BoomBayAh",
+        artist: "BlackPink",
+        img: "blackpink1",
+        src: "blackpink1"
+    },
+    {
+        name: "DDu-Du DDu-Du",
+        artist: "BlackPink",
+        img: "blackpink2",
+        src: "blackpink2"
+    },
+    {
+        name: "Kill-This-Love",
+        artist: "BlackPink",
+        img: "blackpink3",
+        src: "blackpink3"
+    },
+    {
+        name: "Playing-With-Fire",
+        artist: "BlackPink",
+        img: "blackpink4",
+        src: "blackpink4"
+    },
+    {
+        name: "See-U-Later",
+        artist: "BlackPink",
+        img: "blackpink5",
+        src: "blackpink5"
+    },
+    {
+        name: "Stay",
+        artist: "BlackPink",
+        img: "blackpink6",
+        src: "blackpink6"
+    }
+]
